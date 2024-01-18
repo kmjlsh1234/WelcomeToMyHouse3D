6ab27cd9cd0b2c0837a5607a6261b539bb218673
suhanLee <kmjlsh1234@xosoft.kr>
2024-01-18 18:29:21 +0900

    아이템 데이터 istrigger추가

--- WelcomeToMyHouse3D/Assets/scripts/Item/Base/ItemData/ItemData.cs
+++ WelcomeToMyHouse3D/Assets/scripts/Item/Base/ItemData/ItemData.cs
@@ -8,18 +8,33 @@ public class ItemData : ScriptableObject
 {
     [Header("아이템 이름 규칙 : 현재 맵_아이템 이름")]
     public string ItemName;
+    
     [Space(10f)]
+    
     [Header("아이템 타입")]
     public ItemType ItemType;
+    
     [Space(10f)]
+
+    [Header("트리거 활성 여부")]
+    public bool IsTrigger;
+    
+    [Space(10f)]
+    
     [Header("선택지 선택 전 문구")]
     public string[] InteractionScript;
+    
     [Space(20f)]
+    
     [Header("선택지 A 문구")]
     public string ChoiceA;
+    
     [Header("선택지 B 문구")]
     public string ChoiceB;
+    
     [Space(20f)]
     [Header("선택지 선택 완료 문구")]
     public string ChoiceFinishScript;
+
+
 }
